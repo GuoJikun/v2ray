a64eca4c6bad56582d4f17eb74983d2d07744c03
GuoJikun <guojikun@y-open.com>
2022-01-26 13:40:40 +0800

    feat:增加自动导入element-plus设置

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <el-config-provider size="small" locale="Zh">
-    <img src="./assets/logo.png" />
-    <div>
-      <p>
-        If Element Plus is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button type="primary">el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <el-config-provider :size="size" :locale="locale">
+    <router-view></router-view>
   </el-config-provider>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import Zh from "element-plus/es/locale/lang/zh-cn";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
-      Zh,
+      locale: zhCn,
+      size: "default",
     };
   },
 };
@@ -32,11 +20,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
 }
 </style>
